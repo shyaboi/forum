a9736b87eff2c95603b50f79f5f7207ebde89edf
Shyaboi <ian.shane.sears@gmail.com>
2020-08-09 16:21:15 -0700

    comments fully working rrreply with custom avatar links

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 const donus = process.env.MONGO_THING;
 var exphbs = require("express-handlebars");
-var helpers = require('handlebars-helpers')();
+var helpers = require("handlebars-helpers")();
 var bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
 // const NewPost = require("./models/models")
@@ -15,36 +15,31 @@ const mongoDB = `mongodb+srv://shyaboi:${donus}@cluster0.zqw64.azure.mongodb.net
 var mongoose = require("mongoose");
 const { json } = require("body-parser");
 var db = mongoose.connection;
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 const { ok } = require("assert");
- 
+
 // Define a custom namespace.  Readers, create your own using something like
 
 var Schema = mongoose.Schema;
-
 
 app.use(express.static("public/views/layouts"));
 
 app.use(express.static(__dirname + "/public"));
 var NewPost = new Schema({
   title: String,
-  avatar:String,
+  avatar: String,
   authour: String,
   date: String,
   comment: String,
   replys: Array,
-  uuid:String,
-  opID:String,
-  ip:String
+  uuid: String,
+  opID: String,
+  ip: String,
 });
-
-
 
 // -----------------------------------------------------date constructor
 
-
 // console.log(longDate);
-
 
 // -----------------------------------------------------date constructor
 
@@ -61,67 +56,99 @@ app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 app.use(bodyParser.json()); // to support JSON bodies
 app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
-var moment = require('moment'); // require
+var moment = require("moment"); // require
 
 app.get("/forum", (request, response) => {
- console.log(typeof(longDate))
+  console.log(typeof longDate);
 
-const ip = request.header('x-forwarded-for') || request.connection.remoteAddress;
-const ip2 = request.headers['x-forwarded-for'] || 
-request.connection.remoteAddress || 
-request.socket.remoteAddress ||
-(request.connection.socket ? request.connection.socket.remoteAddress : null);
-console.log("ip1:"+ip)
-console.log("ip2:"+ip2)
-    const getAll = () => {
+  const ip =
+    request.header("x-forwarded-for") || request.connection.remoteAddress;
+  const ip2 =
+    request.headers["x-forwarded-for"] ||
+    request.connection.remoteAddress ||
+    request.socket.remoteAddress ||
+    (request.connection.socket
+      ? request.connection.socket.remoteAddress
+      : null);
+  console.log("ip1:" + ip);
+  console.log("ip2:" + ip2);
+  const getAll = () => {
     MongoClient.connect(
-        mongoDB,
-        { useNewUrlParser: true, useUnifiedTopology: true },
-        function (err, db) {
-          if (err) throw err;
-          var dbo = db.db("donu");
-          var mysort = { date: -1 };
-          dbo
-            .collection("Forum")
-            .find({})
-            .sort(mysort)
-            .toArray(function (err, result) {
-              if (err) throw err;
-              // for (let i = 0; i < result.length; i++) {
-                  //   const all = result[i];
-                  // console.log("\x1b[35m", element.name);
-                  // var getAl = all.name
-                  // console.log(getAl)
-                  const results = result.map((wall) => {
-                      return wall;
-                    });
-                    const fileName = results;
-                    // for (let i = 0; i < fileName.length; i++) {
-                    //   const element = JSON.stringify(fileName[i].comment);
-                    //   console.log(element)
-                    // }
-                    
-  // }
-              db.close();
-              response.render(`forum`, {
-                fileName: fileName,
-              });
+      mongoDB,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("donu");
+        var mysort = { date: -1 };
+        dbo
+          .collection("Forum")
+          .find({})
+          .sort(mysort)
+          .toArray(function (err, result) {
+            if (err) throw err;
+            // for (let i = 0; i < result.length; i++) {
+            //   const all = result[i];
+            // console.log("\x1b[35m", element.name);
+            // var getAl = all.name
+            // console.log(getAl)
+            const results = result.map((wall) => {
+              return wall;
             });
-        }
-      );
-    };
-    getAll();
-    // console.log(ok)
-  });
+            const fileName = results;
+            // for (let i = 0; i < fileName.length; i++) {
+            //   const element = JSON.stringify(fileName[i].comment);
+            //   console.log(element)
+            // }
 
-  
+            // }
+            db.close();
+            response.render(`forum`, {
+              fileName: fileName,
+            });
+          });
+      }
+    );
+  };
+  getAll();
+  // console.log(ok)
+});
 
-  app.get("/post:postid?", (request, response) => {
-    
-    // console.log(request.query.name)
-    let keyParam = request.params.postid;
+app.get("/post:postid?", (request, response) => {
+  // console.log(request.query.name)
+  let keyParam = request.params.postid;
 
-console.log(keyParam)
+  console.log(keyParam);
+  MongoClient.connect(
+    mongoDB,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("donu");
+      var mysort = { date: 1 };
+      dbo
+        .collection("Forum")
+        .find({ uuid: keyParam })
+        .sort(mysort)
+        .toArray(function (err, result) {
+          if (err) throw err;
+          const results = result.map((post) => {
+            return post;
+          });
+          const postData = results[0];
+          // const postReply = results[0].replys
+
+          // console.log(postData)
+
+          response.render(`post`, {
+            postData: postData,
+          });
+        });
+    }
+  );
+});
+
+app.get("/", (request, response) => {
+  const getAll = () => {
     MongoClient.connect(
       mongoDB,
       { useNewUrlParser: true, useUnifiedTopology: true },
@@ -131,93 +158,61 @@ console.log(keyParam)
         var mysort = { date: 1 };
         dbo
           .collection("Forum")
-          .find({uuid:keyParam})
+          .find({
+            replys: {
+              $elemMatch: { uuuid: "0372d7e9-a6d0-4de2-b0e3-b0e517a142e4" },
+            },
+          })
           .sort(mysort)
           .toArray(function (err, result) {
             if (err) throw err;
-            const results = result.map((post) => {
-              return post;
+            for (let i = 0; i < result.length; i++) {
+              var all = result[i];
+              console.log("\x1b[35m", all.replys);
+            }
+            // const results = result.map((wall) => {
+            //   return wall;
+            // });
+            // console.log(wall)
+
+            const fileName = result;
+            // }
+            db.close();
+            response.render(`home`, {
+              fileName: fileName,
+              all: all,
             });
-            const postData = results[0]
-            // const postReply = results[0].replys
-
-     
-                
-                // console.log(postData)
-    
-           
-                response.render(`post`, {
-                    postData: postData,
-                  });
-  });
-  })
-})
-
-app.get("/", (request, response) => {
-
-    const getAll = () => {
-    MongoClient.connect(
-        mongoDB,
-        { useNewUrlParser: true, useUnifiedTopology: true },
-        function (err, db) {
-          if (err) throw err;
-          var dbo = db.db("donu");
-          var mysort = { date: 1 };
-          dbo
-            .collection("Forum")
-            .find(
-              {replys:{$elemMatch:{uuuid:"0372d7e9-a6d0-4de2-b0e3-b0e517a142e4"}}}
-              )
-            .sort(mysort)
-            .toArray(function (err, result) {
-              if (err) throw err;
-              for (let i = 0; i < result.length; i++) {
-                    var all = result[i];
-                  console.log("\x1b[35m", all.replys);
-              }
-                  // const results = result.map((wall) => {
-                  //   return wall;
-                  // });
-                  // console.log(wall)
-                  
-              const fileName = result;
-              // }
-              db.close();
-              response.render(`home`, {
-                fileName: fileName,
-                all:all,
-              });
-            });
-        }
-      );
-    };
-    getAll();
-    // console.log(ok)
-  });
+          });
+      }
+    );
+  };
+  getAll();
+  // console.log(ok)
+});
 
 app.get("/newpost", (request, response) => {
   response.render(`newpost`);
 });
 app.post("/postpost", (request, response) => {
-
-  const longDate = moment().format()
-  let ip = request.header('x-forwarded-for') || request.connection.remoteAddress;
+  const longDate = moment().format();
+  let ip =
+    request.header("x-forwarded-for") || request.connection.remoteAddress;
 
   const title = request.body.title;
   const comment = request.body.comment;
   const author = request.body.author;
   const uuuid = uuidv4();
-  const avatar = request.body.avatar
-  console.log(longDate)
+  const avatar = request.body.avatar;
+  console.log(longDate);
   const mongoModle = new Model({
     title: `${title}`,
-    avatar:avatar,
+    avatar:avatar||"https://placekitten.com/96/139",
     authour: author,
     date: longDate,
     comment: `${comment}`,
     replys: [],
-    uuid:uuuid,
-    ip:ip
+    uuid: uuuid,
+    ip: ip,
   });
 
   MongoClient.connect(
@@ -234,120 +229,112 @@ app.post("/postpost", (request, response) => {
       });
     }
   );
-setTimeout(() => {
+  setTimeout(() => {
     response.redirect(`/post${uuuid}`);
-    
-}, 300);
+  }, 300);
 });
 
+app.post("/replypost", (response, request) => {
+  console.log(response.body);
+  let keyParam = response.body.opID;
+  // const uuid = response.body.uuid
+  const avatar = response.body.avatar;
 
+  const longDate = moment().format();
+  MongoClient.connect(
+    mongoDB,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("donu");
+      var mysort = { date: 1 };
+      dbo.collection("Forum").updateOne(
+        { uuid: keyParam },
+        {
+          $push: {
+            replys: {
+              author: response.body.authorr,
+              avatar: avatar || "https://placekitten.com/96/139",
+              comment: response.body.commentr,
+              opID: keyParam,
+              uuuid: uuidv4(),
+              date: longDate,
+              replis: [],
+            },
+          },
+        }
+      );
+      console.log("comment added to: " + keyParam);
+      //   .sort(mysort)
+      //   .toArray(function (err, result) {
+      //     if (err) throw err;
+      //     const results = result.map((post) => {
+      //       return post;
+      //     });
+      //     const postData = results[0]
+      //     // const postReply = results[0].replys
 
+      //         console.log(postData)
 
-app.post("/replypost", (response,request)=> {   
-     console.log(response.body)
-    let keyParam = response.body.opID;
-    // const uuid = response.body.uuid
-    const avatar = response.body.avatar
- 
-    const longDate = moment().format()
-    MongoClient.connect(
-      mongoDB,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("donu");
-        var mysort = { date: 1 };
-        dbo
-          .collection("Forum")
-          .updateOne({uuid:keyParam}, { $push: { replys: 
-            {   
-              author:response.body.authorr,
-              avatar:avatar||"https://placekitten.com/96/139",
-                comment:response.body.commentr,
-                opID:keyParam,
-                uuuid:uuidv4(),
-                date:longDate,
-                replis:[]
-                } }})
-          console.log("comment added to: " + keyParam)
-        //   .sort(mysort)
-        //   .toArray(function (err, result) {
-        //     if (err) throw err;
-        //     const results = result.map((post) => {
-        //       return post;
-        //     });
-        //     const postData = results[0]
-        //     // const postReply = results[0].replys
+      //   });
+    }
+  );
+  setTimeout(() => {
+    request.redirect(`/post${keyParam}`);
+  }, 300);
+});
 
-     
-                
-        //         console.log(postData)
-    
-           
-        //   });
-    })
-    setTimeout(() => {
-        request.redirect(`/post${keyParam}`);
-        
-    }, 300);  
-})
-
-
-app.post("/replyreplypost", (response,request)=> { 
- 
-  const longDate = moment().format()
-  const don = response.body.uuuid.split("+")  ;
-  const donus = don[0]; 
+app.post("/replyreplypost", (response, request) => {
+  const longDate = moment().format();
+  const don = response.body.uuuid.split("+");
+  const donus = don[0];
   const danus = don[1];
-     console.log(danus)
-    let keyParam = response.body.uuuid;
+  console.log(danus);
+  let keyParam = response.body.uuuid;
 
-// console.log(keyParam)
-    MongoClient.connect(
-      mongoDB,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      function (err, db) {
-        if (err) throw err;
-        var dbo = db.db("donu");
-        var mysort = { date: 1 };
-        dbo
-          .collection("Forum")
-          .updateOne({replys:{$elemMatch:{uuuid:donus}}}, { $push: { "replys.$.replis": 
-            {   
-                  author:response.body.authorr,
-                  comment:response.body.commentr,
-                  opID:donus,
-                  oppID:danus,
-                  uuuuid:uuidv4(),
-                  date:longDate,
-                } }})
-          // console.log("comment added to: " + keyParam)
-        //   .sort(mysort)
-        //   .toArray(function (err, result) {
-        //     if (err) throw err;
-        //     const results = result.map((post) => {
-        //       return post;
-        //     });
-        //     const postData = results[0]
-        //     // const postReply = results[0].replys
+  const avatar = response.body.avatar
+  MongoClient.connect(
+    mongoDB,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err, db) {
+      if (err) throw err;
+      var dbo = db.db("donu");
+      var mysort = { date: 1 };
+      dbo.collection("Forum").updateOne(
+        { replys: { $elemMatch: { uuuid: donus } } },
+        {
+          $push: {
+            "replys.$.replis": {
+              author: response.body.authorr,
+              avatar: avatar || "https://placekitten.com/96/139",
+              comment: response.body.commentr,
+              opID: donus,
+              oppID: danus,
+              uuuuid: uuidv4(),
+              date: longDate,
+            },
+          },
+        }
+      );
+      // console.log("comment added to: " + keyParam)
+      //   .sort(mysort)
+      //   .toArray(function (err, result) {
+      //     if (err) throw err;
+      //     const results = result.map((post) => {
+      //       return post;
+      //     });
+      //     const postData = results[0]
+      //     // const postReply = results[0].replys
 
-     
-                
-        //         console.log(postData)
-    
-           
-        //   });
-    })
-    setTimeout(() => {
-        request.redirect(`/post${danus}`);
-        
-    }, 300);  
-})
+      //         console.log(postData)
 
-
-
-
-
+      //   });
+    }
+  );
+  setTimeout(() => {
+    request.redirect(`/post${danus}`);
+  }, 300);
+});
 
 app.listen(PORT);
 console.log("server started on " + PORT);
