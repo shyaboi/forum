db5c841181edc1565f87007c1850563f80b72ad7
Shyaboi <ian.shane.sears@gmail.com>
2020-08-12 21:23:22 -0700

    region had country flag in it

--- server.js
+++ server.js
@@ -313,7 +313,7 @@ app.post("/newRegionPost", (request, response) => {
     console.log(JSON.parse(body));
   bod = JSON.parse(body);
   flag = bod.country_flag;
-  region = bod.country_flag
+  region = bod.region
   console.log(bod.country_flag,bod.region)
   });
 
